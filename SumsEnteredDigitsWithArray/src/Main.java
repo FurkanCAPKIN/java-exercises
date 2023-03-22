0c8ed46fad8c0e0eedd64316eb747eaeb3640300
Furkan Çapkın <92672616+FurkanCAPKIN@users.noreply.github.com>
2023-03-22 07:33:05 +0300

    Add files via upload

---
+++ SumsEnteredDigitsWithArray/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the digits(if you want the stop enter 0): ");
+        int i;
+        int sum=0;
+        int[] number = new int[100];
+        for (i=0;i<100;i++){
+            number[i] = input.nextInt();
+            if (number[i]==0){
+                break;
+            }else {
+                sum += number[i];
+            }
+        }
+
+        System.out.println("Sum of the entered numbers: "+sum);
+
+
+
+    }
+}

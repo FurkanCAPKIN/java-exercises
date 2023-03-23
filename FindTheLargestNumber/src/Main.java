087b68795936e46dcead1725309d757d905b8cf1
Furkan Çapkın <92672616+FurkanCAPKIN@users.noreply.github.com>
2023-03-23 23:54:55 +0300

    Add files via upload

---
+++ FindTheLargestNumber/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double maxDouble1;
+        double maxDouble2;
+        maxDouble1 = returnMax(34, 28.8, 16.6, 53, 55.5);
+        maxDouble2 = returnMax(new double[]{1, 2, 3});
+
+        System.out.println("The max value is " + maxDouble1);
+        System.out.println("The max value is " + maxDouble2);
+
+    }
+
+    public static double returnMax(double... numbers) {
+        if (numbers.length == 0) {
+            System.out.println("No argument passed");
+            return 0.0;
+        }
+
+        double result = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++)
+            if (numbers[i] > result)
+                result = numbers[i];
+
+        return result;
+
+    }
+
+
+}
+
+/*
+
+ */
+

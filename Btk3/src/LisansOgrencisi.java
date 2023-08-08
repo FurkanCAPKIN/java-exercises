76cd4532b0765abb62c8e1c7d6514ba700643714
Furkan Çapkın <92672616+FurkanCAPKIN@users.noreply.github.com>
2023-08-08 23:02:16 +0300

    Add files via upload

---
+++ Btk3/src/LisansOgrencisi.java
@@ -0,0 +1,26 @@
+public class LisansOgrencisi extends ogrenci
+{
+
+
+    LisansOgrencisi (String name, int no, double test1, double test2, double test3)
+    {
+          this.name = name;
+          this.no = no;
+          this.test1 = test1;
+          this.test2 = test2;
+          this.test3 = test3;
+    }
+
+    String notlandirma (double ort)
+    {
+        if ( ort >= 70 )
+        {
+            return "Geçti";
+        }else
+        {
+            return "Kaldı";
+        }
+
+    }
+
+}
---
+++ Btk3/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import Mezun.mezunOgrenci;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lisans Öğrencisi oluşturmak için -> 1\n" +
+                "Yüksek Lisans Öğrencisi oluşturmak için -> 2 ");
+        int tercih = input.nextInt();
+
+        if ( tercih == 1)
+        {
+            System.out.println("Öğrenci ismi: ");
+            String isim = input.next();
+            System.out.println("Öğrenci no: ");
+            int no = input.nextInt();
+            System.out.println("Öğrenci test1 notu: ");
+            double test1 = input.nextDouble();
+            System.out.println("Öğrenci test2 notu: ");
+            double test2 = input.nextDouble();
+            System.out.println("Öğrenci test3 notu: ");
+            double test3 = input.nextDouble();
+
+            ogrenci lo = new LisansOgrencisi(isim,no,test1,test2,test3);
+
+            System.out.println("Öğrenci not ortalamasi: "+lo.ortHesaplama(test1,test2,test3));
+            System.out.println("Öğrenci harf ortalaması: "+lo.notlandirma(lo.ortHesaplama(test1,test2,test3)));
+
+        }else if ( tercih == 2)
+        {
+            System.out.println("Öğrenci ismi: ");
+            String isim = input.next();
+            System.out.println("Öğrenci no: ");
+            int no = input.nextInt();
+            System.out.println("Öğrenci test1 notu: ");
+            double test1 = input.nextDouble();
+            System.out.println("Öğrenci test2 notu: ");
+            double test2 = input.nextDouble();
+            System.out.println("Öğrenci test3 notu: ");
+            double test3 = input.nextDouble();
+
+            ogrenci ylo = new YuksekLisansOgrencisi(isim,no,test1,test2,test3);
+
+            System.out.println("Öğrenci not ortalamasi: "+ylo.ortHesaplama(test1,test2,test3));
+            System.out.println("Öğrenci harf ortalaması: "+ylo.notlandirma(ylo.ortHesaplama(test1,test2,test3)));
+
+
+        }else
+        {
+            System.out.println("Yanlış tercih yaptınız.");
+        }
+
+
+
+        mezunOgrenci mezunOgrenci = new mezunOgrenci("Furkan",2024,95);
+
+
+    }
+}
